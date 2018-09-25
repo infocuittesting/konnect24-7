aab3dc24cd0e2970c2d85281af2f4db724db6c35
infocuittesting <infocuit.testing@gmail.com>
2018-09-25 15:34:07 +0530

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { AdminModule } from './admin/admin.module';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
