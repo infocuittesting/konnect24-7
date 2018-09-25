91192218ee06deb97a4efd7a8c1faf313f191acf
infocuittesting <infocuit.testing@gmail.com>
2018-09-25 12:19:29 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,7 @@ import { AmChartsModule } from "amcharts3-angular2";
 import { AppComponent } from './app.component';
 import { StarterComponent } from './starter/starter.component';
 
-import { AdminComponent } from './admin/admin.component';
-import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
-import { AdminLeftSideComponent } from './admin/admin-left-side/admin-left-side.component';
-import { AdminContentComponent } from './admin/admin-content/admin-content.component';
-import { AdminFooterComponent } from './admin/admin-footer/admin-footer.component';
-import { AdminControlSidebarComponent } from './admin/admin-control-sidebar/admin-control-sidebar.component';
-import { AdminDashboard1Component } from './admin/admin-dashboard1/admin-dashboard1.component';
+
 
 
 // import { DateFormatPipe } from './date-format.pipe';
