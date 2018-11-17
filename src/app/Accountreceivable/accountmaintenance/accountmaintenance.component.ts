6541d37d969094b8f495afbd1bf20b8fb465086a
ingitfocuittesting <infocuit.testing@gmail.com>
2018-11-17 12:12:25 +0530

    account receivable

--- src/app/Accountreceivable/accountmaintenance/accountmaintenance.component.ts
+++ src/app/Accountreceivable/accountmaintenance/accountmaintenance.component.ts
@@ -16,6 +16,9 @@ export class AccountmaintenanceComponent implements OnInit {
   constructor(private AccountmaintenanceService: AccountmaintenanceService, public session:SessionStorageService,private route:Router,private toasterService:ToasterServiceService) { }
 
   public ac_maintain_tabl=[];
+  public market_val=[];
+  public source_val=[];
+  public room_cls_val=[];
   public enablebut=true;
   ngOnInit() {
     this.AccountmaintenanceService.account_table()
@@ -23,7 +26,27 @@ export class AccountmaintenanceComponent implements OnInit {
      this.ac_maintain_tabl=resp.ReturnValue;
      console.log("account maintain table valuessssssss",this.ac_maintain_tabl)
    });
-  }
+
+
+   this.AccountmaintenanceService.market_dropdown()
+   .subscribe((resp: any) => { 
+    this.market_val=resp.ReturnValue;
+    console.log("marketdropdown valuess",this.market_val)
+  });
+
+  this.AccountmaintenanceService.source_dropdown()
+  .subscribe((resp: any) => { 
+   this.source_val=resp.ReturnValue;
+   console.log("sourcedropdown valuess",this.source_val)
+ });
+
+ this.AccountmaintenanceService.room_class_dropdown()
+ .subscribe((resp: any) => { 
+  this.room_cls_val=resp.ReturnValue;
+  console.log("roomdropdown valuess",this.room_cls_val)
+});
+
+}
 
   selectindex
   selectMembersEdit(index,value){
@@ -33,6 +56,25 @@ export class AccountmaintenanceComponent implements OnInit {
     this.session.store("invoice_no",value.invoice_no)
     
 
+  }
+newinvoice={}
+  newinvoicefun(newinvoice){
+console.log("new invoice values",newinvoice)
+this.AccountmaintenanceService.insert_accountin(newinvoice)
+.subscribe((resp: any) => {
+  var insertresp=resp.ReturnCode
+  if(insertresp=='RIS'){
+    var message="New Invoice Created Successfully"
+    this.toasterService.success(message);
+  }
+
+  // refresh traces table records //
+  this.AccountmaintenanceService.account_table()
+  .subscribe((resp: any) => { 
+   this.ac_maintain_tabl=resp.ReturnValue;
+   console.log("account maintain table value",this.ac_maintain_tabl)
+ });
+ });
   }
 
 }
--- src/app/Accountreceivable/accountmaintenance/accountmaintenance.service.ts
+++ src/app/Accountreceivable/accountmaintenance/accountmaintenance.service.ts
@@ -16,9 +16,54 @@ export class AccountmaintenanceService {
        return this.http.post('http://hotel360.herokuapp.com/HOTEL_AR_POST_SELECT_AccountInvoice',body)
           .map(this.extractData)
      }
+  //  market dropdown value
+    market_dropdown():  Observable<object[]> {    
+         return this.http.get('https://hotel360.herokuapp.com/Hotel_RES_GET_SELECT_Market')
+            .map(this.extractData)
+       }
    
-   
-    
+ //  source dropdown value
+    source_dropdown():  Observable<object[]> {    
+        return this.http.get('https://hotel360.herokuapp.com/Hotel_RES_GET_SELECT_Source')
+           .map(this.extractData)
+      }
+
+     //  payment dropdown value
+     payment_dropdown():  Observable<object[]> {    
+      return this.http.get('https://hotel360.herokuapp.com/Hotel_RES_GET_SELECT_Payment')
+         .map(this.extractData)
+    }
+
+      //  payment dropdown value
+      currency_dropdown():  Observable<object[]> {    
+      return this.http.get('https://hotel360.herokuapp.com/Profile/profilecurrency')
+          .map(this.extractData)
+    }
+
+//  room class dropdown value
+      room_class_dropdown():  Observable<object[]> {   
+        const headers = new Headers({'Content-Type':'application/json'})
+        const options = new RequestOptions({ headers: headers }) 
+        return this.http.post('https://hotel360.herokuapp.com/Select_Class',options)
+           .map(this.extractData)
+      }
+// insert new accountinvoice
+      insert_accountin(newinvoice):  Observable<object[]> {    
+        let body={
+          "account_number":this.session.retrieve("account_number"),          
+          "account_name":newinvoice.guest,
+          "market_id":String(newinvoice.market_code),
+          "source_id":String(newinvoice.source_code),
+          "room_class_id":String(newinvoice.roomclass_code),
+          "invoice_supplement":newinvoice.supp,
+          "reference":newinvoice.refr
+          
+          }
+          
+           return this.http.post('http://hotel360.herokuapp.com/HOTEL_AR_POST_INSERT_AccountInvoice ',body)
+              .map(this.extractData)
+         }
+       
      private extractData(res: Response) {
        //alert('hai20')
        console.log('res========---===='+res);
